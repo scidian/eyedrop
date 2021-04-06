2615dd8d30f8401b7ffbdd4723a3369a8ba27c3b
Stephens Nunnally <stevinz@gmail.com>
2021-04-06 16:49:27 -0400

    Include

--- engine/app/RenderContext.h
+++ engine/app/RenderContext.h
@@ -9,6 +9,9 @@
 #ifndef DR_RENDER_CONTEXT_H
 #define DR_RENDER_CONTEXT_H
 
+// Includes
+#include "engine/app/image/Color.h"
+
 // Shaders
 #include "engine/scene3d/shaders/BasicShader.glsl.h"
 
@@ -28,8 +31,8 @@ public:
 public:
     // Render Context Variables
     sg_pass_action      pass_action     {};     
-    sg_pipeline         pipeline        {};     // Shader... Pipeline holds shader, vertex shader attribute type, primitive type, index type, cull mode, depth info, blend mode
-    sg_bindings         bindings        {};     // Mesh...   Bindings hold vertex buffers, index buffers, and fragment shader images
+    sg_pipeline         pipeline        {};         // Shader... Pipeline holds shader, vertex shader attribute type, primitive type, index type, cull mode, depth info, blend mode
+    sg_bindings         bindings        {};         // Mesh...   Bindings hold vertex buffers, index buffers, and fragment shader images
 
 
     // #################### INTERNAL FUNCTIONS ####################
